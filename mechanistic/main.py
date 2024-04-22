2860f87c7db1393af98671026950e730d6dbe4f0
LingyuLi <lilingyu1874@sjtu.edu.cn>
2024-04-22 17:47:16 +0800

    main

---
+++ mechanistic/main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from interact import Interact
+
+def main():
+    root = tk.Tk()
+    interactive = Interact(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
